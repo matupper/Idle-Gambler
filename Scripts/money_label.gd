7f52c5f2a1d02eb8d8fc3446659e32c76a6e9a67
enjiwood <87221016+enjiwood@users.noreply.github.com>
2024-04-27 22:06:13 -0700

    Score and Setup
    Score UI created and working.
    Globals and upgrade variables set up.
    UI in the works.

---
+++ Scripts/money_label.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@onready var moneyText = $Money
+
+var currMoney = globalVars.money
+
+func _process(_delta):
+	_checkMoney()
+
+func _checkMoney():
+	if currMoney < globalVars.money:
+		currMoney = globalVars.money
+		moneyText.mesh.text = str(currMoney)
+		$AnimationPlayer.play("add_money")
